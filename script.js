aab1fbd91eb0c1df660bfc953b66f851d6019486
oho_tom\tom <oho_cooh@hotmail.com>
2017-03-09 00:36:16 +0900

    ・タブ幅の修正 ・タイル初期化処理を追加

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 (function(){
-  'use strict';
+	'use strict';
 
-  var canvas = document.getElementById('stage');
-  var context;
-  var image;
-  var IMAGE_URL = '15puzzle.png';
+	var canvas = document.getElementById('stage');
+	var context;
+	var image;
+	var IMAGE_URL = '15puzzle.png';
 
-  function drawPuzzle(){
-    context.drawImage(image, 0, 0);
-  }
+	var tiles = [];
+	var ROW_COUNT = 4;
+	var COL_COUNT = 4;
 
-  if(!canvas.getContext){
-    alert('Canvas not supported ...');
-    return;
-  }
-  context = canvas.getContext('2d');
+	function initTiles(){
+		var row, col;
+		for(row = 0; row < ROW_COUNT; row++){
+			tiles[row] = [];
+			for(col = 0; col < COL_COUNT; col++){
+				tiles[row][col] = row*ROW_COUNT + col;
+			}
+		}
+	}
 
-  image = document.createElement('img');
-  image.src = IMAGE_URL;
-  image.addEventListener('load', function(){
-    drawPuzzle();
-  })
+	function drawPuzzle(){
+		context.drawImage(image, 0, 0);
+	}
+
+	if(!canvas.getContext){
+		alert('Canvas not supported ...');
+		return;
+	}
+	context = canvas.getContext('2d');
+
+	image = document.createElement('img');
+	image.src = IMAGE_URL;
+	image.addEventListener('load', function(){
+		initTiles();
+		drawPuzzle();
+	})
 })();
